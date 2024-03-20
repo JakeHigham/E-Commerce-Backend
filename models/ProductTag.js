c9001438fd41dcd9ec609f6039e097d671392b38
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-03-19 23:11:52 -0500

    models folder added
    Added models folder with updated code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -3,10 +3,31 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 ProductTag.init(
   {
-    // define columns
+   
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: Product.id
+    },
+
+   
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: Tag.id
+    }
+    
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
